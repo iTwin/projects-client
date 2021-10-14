6bd3c10d06e918ba5d9215d66c5871bd78db2d11
Spencer Barnes <54333460+SpencerWBarnes@users.noreply.github.com>
2021-10-14 15:37:18 -0500

    Fixed null operator error

--- src/test/TestConfig.ts
+++ src/test/TestConfig.ts
@@ -14,10 +14,9 @@ export class TestConfig {
   /** Login the specified user and return the AuthorizationToken */
   public static async getAccessToken(user: TestUserCredentials = TestUsers.regular): Promise<AccessToken> {
     const debugConfig: TestBrowserAuthorizationClientConfiguration = {
-      clientId: process.env.IMJS_OIDC_BROWSER_TEST_CLIENT_ID ?? "",
-      redirectUri: process.env.IMJS_OIDC_BROWSER_TEST_REDIRECT_URI ?? "",
-      scope: `${process.env.IMJS_OIDC_BROWSER_TEST_SCOPES ?? ""} projects:read`,
-      // scope: process.env.IMJS_OIDC_BROWSER_TEST_SCOPES ?? "",
+      clientId: process.env.IMJS_OIDC_BROWSER_TEST_CLIENT_ID || "",
+      redirectUri: process.env.IMJS_OIDC_BROWSER_TEST_REDIRECT_URI || "",
+      scope: `${process.env.IMJS_OIDC_BROWSER_TEST_SCOPES || ""} projects:read`,
     };
     return getAccessTokenFromBackend(user, debugConfig);
   }
