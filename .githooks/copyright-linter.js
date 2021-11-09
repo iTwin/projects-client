d2e4743c5a21dd9d60d06fb01377b7f1970a4283
Spencer Barnes <54333460+SpencerWBarnes@users.noreply.github.com>
2021-11-09 12:19:38 -0600

    Added method that uses commit --amend

--- .githooks/copyright-linter.js
+++ .githooks/copyright-linter.js
@@ -11,7 +11,7 @@ function getFileNames(stagedFilesOnly) {
 
   // 1. Simplest method:
   // 2. Silently add using single commit:
-  const diffCommand = "git diff --name-only " + (stagedFilesOnly ? "--staged" : "main");
+  // const diffCommand = "git diff --name-only " + (stagedFilesOnly ? "--staged" : "main");
 
 
   // 3. Silently add using separate commit (pre-commit version):
@@ -19,7 +19,8 @@ function getFileNames(stagedFilesOnly) {
 
 
   // 4. Silently add using separate commit (post-commit version):
-  // const diffCommand = "git diff --name-only " + (stagedFilesOnly ? "HEAD~1" : "main");
+  // 5. Use --amend:
+  const diffCommand = "git diff --name-only " + (stagedFilesOnly ? "HEAD~1" : "main");
 
   return child_process.execSync(diffCommand)
     .toString()
@@ -44,8 +45,6 @@ const oldCopyrightBanner = RegExp(
 // If '--branch' is passed-in all files changed since main/master will be linted
 // otherwise only currently staged files will be linted
 const filePaths = getFileNames(!process.argv.includes("--branch"))
-
-console.log(filePaths);
 
 if (filePaths) {
   filePaths.forEach((filePath) => {
